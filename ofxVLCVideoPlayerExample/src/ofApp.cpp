cb577ce909d8fb481eb067fdcd5cc679d8395831
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-06-03 15:38:51 +0200

    Add files via upload

---
+++ ofxVLCVideoPlayerExample/src/ofApp.cpp
@@ -0,0 +1,71 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(255,255,255);
+    ofSetWindowTitle("ofxVLCVideoPlayer example");
+    
+    ofSetFrameRate(60); // if vertical sync is off, we can go a bit fast... this caps the framerate at 60fps.
+    
+    player.loadMovie("data/fingers.mov");
+    player.setLoop(true);
+    thumbnail = player.getThumbnailImage();
+    player.play();
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+   player.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    thumbnail.draw(0, 0, 320, 240);
+    player.draw(0, 0, 320, 240);
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed  (int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased  (int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
