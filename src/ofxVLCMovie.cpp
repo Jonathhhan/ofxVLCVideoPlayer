5247054fa8b11f5295a4343c157deeb67930fe30
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-06-14 19:53:48 +0200

    Update ofxVLCMovie.cpp

--- src/ofxVLCMovie.cpp
+++ src/ofxVLCMovie.cpp
@@ -72,7 +72,7 @@ void ofxVLCMovie::cleanupVLC() {
 
 void ofxVLCMovie::play() {
     if (isLooping) {
-        libvlc_media_add_option(m, "input-repeat=-1");
+        libvlc_media_add_option(m, "input-repeat=65545");
     }
     else {
         libvlc_media_add_option(m, "input-repeat=0");
